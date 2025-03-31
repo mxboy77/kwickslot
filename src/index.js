6bdcecdf2914be0ba4e9d7f8b6f5158df38c44c7
mxboy77 <slabmobile@aol.com>
2025-03-31 11:47:11 -0400

    updates index.js for traffic monitoring

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import Home from './pages/Home';
 import Poll from './pages/Poll';
 import PollResults from './pages/PollResults';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react'; // 👈 Add this line
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/poll/:id" element={<Poll />} />
-      <Route path="/poll/:id/results" element={<PollResults />} /> {/* 👈 This was missing */}
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/poll/:id" element={<Poll />} />
+        <Route path="/poll/:id/results" element={<PollResults />} />
+      </Routes>
+      <Analytics /> {/* 👈 Add this here */}
+    </>
   </Router>
 );
